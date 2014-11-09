a3c51e2d002d2e23a64accaf2a17724b03d0fbe3
Karl Willett <Karl@Karls-Home-iMac-2.local>
2014-11-09 12:44:53 +1100

    added a comment to test commits

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
 
     
     @IBAction func sendMailButtonPressed(sender: UIButton) {
-      
+      // adding a comment here to test commits
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         enterMessageTextField.text = ""
